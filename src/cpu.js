26005c9eb4e5c2a754b776ee3902d16e504ca7c7
Aniket kumar <ani1998ket@gmail.com>
2022-01-19 22:52:11 +0530

    Implements more opcodes

--- src/cpu.js
+++ src/cpu.js
@@ -14,6 +14,8 @@ class CPU{
         this.speaker = speaker;
 
         this.speed = 10;
+
+        this.loadFonts()
     }
 
     loadFonts(){
@@ -77,13 +79,19 @@ class CPU{
             this.PC += 2;
             this.execute(opcode);
         }
+        if( this.delayTimer > 0 ) this.delayTimer--;
+        if( this.soundTimer > 0 ) this.soundTimer--;
         this.renderer.render();
     }
 
     execute( opcode ){
 
-        let x = (opcode & 0x0F00) >> 8;
-        let y = (opcode & 0x00F0) >> 4;
+        let x   = (opcode & 0x0F00) >> 8;
+        let y   = (opcode & 0x00F0) >> 4;
+        let nnn = (opcode & 0xFFF);
+        let kk  = (opcode & 0xFF);
+
+        let temp = null;
         
         switch(opcode & 0xF000){
             case 0x0000:
@@ -97,46 +105,109 @@ class CPU{
                 }
                 break;
             case 0x1000:
-                this.PC = (opcode & 0xFFF);
+                this.PC = nnn;
                 break;
             case 0x2000:
                 this.stack.push( this.PC );
-                this.PC = (opcode & 0xFFF);
+                this.PC = nnn;
                 break;
             case 0x3000:
+                if( V[x] == kk ){
+                    this.PC += 2;
+                }
                 break;
             case 0x4000:
+                if( V[x] != kk ){
+                    this.PC += 2;
+                }
                 break;
             case 0x5000:
+                if( (opcode & 0xF == 0x0) && V[x] == V[y] ){
+                    this.PC += 2;
+                }
                 break;
             case 0x6000:
-                this.V[x] = (opcode & 0xFF)
+                this.V[x] = kk;
                 break;
             case 0x7000:
-                this.V[x] += (opcode & 0xFF);
+                this.V[x] += kk;
                 break;
             case 0x8000:
+                switch( opcode & 0xF ){
+                    case 0x0:
+                        this.V[x] = this.V[y];
+                        break;
+                    case 0x1:
+                        this.V[x] |= this.V[y];
+                        break;
+                    case 0x2:
+                        this.V[x] &= this.V[y];
+                        break;
+                    case 0x3:
+                        this.V[x] ^= this.V[y];
+                        break;
+                    case 0x4:
+                        let sum = this.V[x] + this.V[y];
+                        this.V[0xF] = 0;
+                        if( sum > 255 ){
+                            this.V[0xF] = 1;
+                        }
+                        this.V[x] = sum;
+                        break;
+                    case 0x5:
+                        let diff = this.V[x] - this.V[y];
+                        this.V[0xF] = 0;
+                        if( this.V[x] > this.V[y] ){
+                            this.V[0xF] = 1; 
+                        }
+                        this.V[x] = diff;
+                        break;
+                    case 0x6:
+                        this.V[0xF] = this.V[x] & 0x1;
+                        this.V[x] = this.V[x] >> 1;
+                        break;
+                    case 0x7:
+                        diff = this.V[y] - this.V[x];
+                        this.V[0xF] = 0;
+                        if( this.V[y] > this.V[x] ){
+                            this.V[0xF] = 1; 
+                        }
+                        this.V[x] = diff;
+                        break;
+                    case 0xE:
+                        this.V[0xF] = this.V[x] & 0x80;
+                        this.V[x] = this.V[x] << 1;
+                        break;
+                        
+                }
                 break;
             case 0x9000:
+                if( this.V[x] != this.V[y] ){
+                    this.PC += 2;
+                }
                 break;
             case 0xA000:
                 this.I = (opcode & 0xFFF);
                 break;
             case 0xB000:
+                this.PC = nnn + this.V[0];
                 break;
             case 0xC000:
+                let random = Math.floor(Math.random() * 0x100 );
+                this.V[x] = random & kk;
                 break;
             case 0xD000:
-                let n = opcode & 0xF;
+                let sprite_height   = opcode & 0xF;
+                let sprite_width    = 8;
 
                 this.V[0xF] = 0;
 
-                for( let i = 0; i < n; i++ ){
-                    let row = this.RAM[this.I + i];
-                    for( let j = 0; j < 8; j++ ){
-                        let value = row & (1 << j);
+                for( let r = 0; r < sprite_height; r++ ){
+                    let row = this.RAM[this.I + r];
+                    for( let c = 0; c < sprite_width; c++ ){
+                        let value = row & (1 << c);
                         if(value){
-                            if(this.renderer.setPixel(this.V[x] + (7-j), this.V[y] + i)){
+                            if(this.renderer.setPixel(this.V[x] + (7-c), this.V[y] + r)){
                                 this.V[0xF] = 1;
                             }
                         }
@@ -144,8 +215,54 @@ class CPU{
                 }
                 break;
             case 0xE000:
+                switch( opcode & 0xFF ){
+                    case 0x9E:
+                        //...some code here...
+                        break;
+                    case 0xA1:
+                        //...some code here...
+                        break;
+                } 
                 break;
             case 0xF000:
+                switch( opcode & 0xFF ){
+                    case 0x07:
+                        this.V[x] = this.delayTimer;
+                        break;
+                    case 0x0A:
+                        //...some code here...
+                        break;
+                    case 0x15:
+                        this.delayTimer = this.V[x];
+                        break;
+                    case 0x18:
+                        this.soundTimer = this.V[x];
+                        break;
+                    case 0x1E:
+                        this.I += this.V[x];
+                        break;
+                    case 0x29:
+                        this.I = this.V[x] * 5
+                        break
+                    case 0x33:
+                        temp = this.V[x];
+                        this.RAM[this.I] = Math.floor(temp / 100);
+                        temp %= 100;
+                        this.RAM[this.I+1] = Math.floor( temp / 10 );
+                        temp %= 10;
+                        this.RAM[this.I+2] = Math.floor(temp);
+                        break;
+                    case 0x55:
+                        for( let i = 0; i <= x; i++ ){
+                            this.RAM[this.I + i] = this.V[i];
+                        }
+                        break;
+                    case 0x65:
+                        for( let i = 0; i <= x; i++ ){
+                            this.V[i] = this.RAM[this.I + i];
+                        }
+                        break;
+                } 
                 break;
             default:
                 throw new Error('Unknown opcode : ' + opcode );
